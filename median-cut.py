f2e53c1db37a8854b61e0f5cb15b8741c6d26182
fifiten82726 <fifiteen82726@gmail.com>
2019-03-31 20:52:54 -0400

    compute mean

--- median-cut.py
+++ median-cut.py
@@ -7,34 +7,50 @@ class MC():
     self.img = img
     self.rows, self.cols, color = img.shape
     self.img_a = []
-    self.depth = 0
-    self.mapping = {1: }
-    self.q_image = {1: }
+    self.mapping = {1: {}, 2: {}, 4: {}, 8: {}}
+    self.q_image = {1: np.copy(img), 2: np.copy(img), 4: np.copy(img), 8: np.copy(img)}
 
     for i in xrange(self.rows):
       for j in xrange(self.cols):
         self.img_a.append(img[i][j])
 
-  def median_cut(self, img_array, bits):
+  def median_cut(self):
     img_array = self.img_a
-    np.amax(img_array, axis=0) - np.amin(img_array, axis=0)
+    # current_cut, target_cut
+    self.helper(img_array, 0, 1)
+    # self.generate_compressive_image()
 
-  def helper(self, current_cut, target):
-    if current_cut >= target
+  def helper(self, image_arr, current_cut, target):
+    if current_cut >= target:
       return
+    current_cut += 1
 
-    self.sort_by_max_difference_dimension(self, image_arr)
+    self.sort_by_max_difference_dimension(image_arr)
+    lens = len(image_arr)
+    left = image_arr[0:lens/2]
+    right = image_arr[lens/2:lens]
+    if current_cut in [1,2,4,8]:
+      self.compute_mean(left, current_cut)
+      self.compute_mean(right, current_cut)
 
+    self.helper(left, current_cut, target)
+    self.helper(right, current_cut, target)
 
   def sort_by_max_difference_dimension(self, image_arr):
-    diff_a = np.amax(img_array, axis=0) - np.amin(img_array, axis=0)
+    diff_a = np.amax(image_arr, axis=0) - np.amin(image_arr, axis=0)
     index = np.argmax(diff_a)
-    sort(image_arr, key=lambda x:x[index])
+    image_arr.sort(key=lambda x: x[index])
+
+  def compute_mean(self, image_arr, current_cut):
+    mean = np.mean(image_arr, axis=0)
+    for pixel in image_arr:
+      self.mapping[current_cut][tuple(pixel)] = mean
 
 
-image = np.asarray(Image.open('img/t1.jpg').convert('RGB'))
+
+image = np.asarray(Image.open('img/4pixel-image.bmp').convert('RGB'))
 bits = [1,2,4,8]
-MC(image).median_cut([1])
+MC(image).median_cut()
 
 
 
